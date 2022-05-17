cd51ec7cd1a091dc227e140b38ba358c6350d0c4
Loïc <lbertran@student.42lyon.fr>
2022-05-17 19:01:13 +0200

    ?? mais c trop bien

--- main.cpp
+++ main.cpp
@@ -9,9 +9,29 @@ int main() {
 	ft::vector<int> vec;
 	std::vector<int> stdvec;
 
-	stdvec.push_back(20);
-	vec.push_back(20);
 
-	cout << stdvec.at(0) << endl;
-	cout << vec.at(0) << endl;
+	for (int i = 0; i < 6; i++) {
+		stdvec.push_back(i * 10);
+		vec.push_back(i * 10);
+		cout << stdvec.at(i) << endl;
+		cout << vec.at(i) << endl;
+	}
+
+	stdvec.resize(3);
+	vec.resize(3);
+	
+	cout << stdvec.size() << endl;
+	cout << vec.size() << endl;
+
+	cout << stdvec.at(2) << endl;
+	cout << vec.at(2) << endl;
+
+	cout << stdvec.back() << endl;
+	cout << vec.back() << endl;
+
+	stdvec.pop_back();
+	vec.pop_back();
+
+	cout << stdvec.back() << endl;
+	cout << vec.back() << endl;
 }
--- vector.hpp
+++ vector.hpp
@@ -36,10 +36,10 @@ namespace ft {
 		private:
 
 			void _check_capacity(size_type needed) {
-				if (_size != _capacity) return;
+				if (needed <= _capacity - _size) return;
 
 				if (_size + needed > max_size())
-					throw std::length_error("vector::max_capacity_reached");
+					throw std::length_error("ft::vector::max_capacity_reached");
 				else if (_size + needed > (_capacity * 2))
 					reserve(_size + needed);
 				else
@@ -81,35 +81,7 @@ namespace ft {
 
 			//Functions
 
-			template <class InputIterator>
-			void assign (InputIterator first, InputIterator last) {
-				if (_capacity < last - first) {
-					clear();
-					_alloc.deallocate(_data, _capacity);
-				}
-				_alloc.allocate(last - first);
-				while (first != last) {
-					_alloc.construct(_data + _size, *first);
-					first++;
-				}
-			}
-
-
-			void assign (size_type n, const value_type& val);
-
-			void clear () {
-				for (size_t i = 0; i < _size; i++)
-					_alloc.deconstruct(_data[i]);
-				_size = 0;
-			}
-
-			void push_back (const value_type &val) {
-				_check_capacity(_size + 1);
-				_alloc.construct(_data + _size, val);
-				_size++;
-			}
-
-			void reserve (size_type new_capacity) {
+			void 		reserve (size_type new_capacity) {
 				if (new_capacity <= _capacity) return;
 
 				T* new_data = _alloc.allocate(new_capacity);
@@ -122,14 +94,84 @@ namespace ft {
 				_data = new_data; //set current data to new data
 			}
 
-			reference at (size_type n) {
+			void 		resize (size_type n, value_type val = value_type()) {
+				_check_capacity(n);
+				if (n < _size) {
+					for (size_type i = n; i > _size; i--)
+						_alloc.destroy(_data + (i - 1));
+				} else if (n > _size) {
+					for (size_type i = _size; i < n; i++)
+						push_back(val);
+				}
+				_size = n;
+			}
+
+			template <class InputIterator>
+			void 		assign (InputIterator first, InputIterator last) {
+				if (_capacity < last - first) {
+					clear();
+					_alloc.deallocate(_data, _capacity);
+				}
+				_alloc.allocate(last - first);
+				while (first != last) {
+					_alloc.construct(_data + _size, *first);
+					first++;
+				}
+			}
+
+
+			void 		assign (size_type n, const value_type& val);
+
+			void 		clear () {
+				for (size_t i = 0; i < _size; i++)
+					_alloc.deconstruct(_data[i]);
+				_size = 0;
+			}
+
+			void		 push_back (const value_type &val) {
+				_check_capacity(_size + 1);
+				_alloc.construct(_data + _size, val);
+				_size++;
+			}
+
+			void		pop_back() {
+				if (empty()) return;
+				_alloc.destroy(_data + (--_size));
+			}
+
+
+			reference 	operator[] (size_type n) {
+				return _data[n];
+			}
+			
+			reference 	at (size_type n) {
 				if (n >= _size)
-					throw std::out_of_range("vector::out_of_bounds");
+					throw std::out_of_range("ft::vector::out_of_range");
 				return _data[n];
 			}
 
-			size_type max_size() {
+			bool		empty() const {
+				return _size == 0;
+			}
+
+			size_type	max_size() const {
 				return 4611686018427387903;
+			}
+			
+			size_type	size() const {
+				return _size;
+			}
+
+			size_type	capacity() const {
+				return _capacity;
+			}
+
+			reference 	back() {
+				return _data[_size - 1];
+			}
+
+			reference	front() {
+				return _data[0];
 			}
 
 			// vector		&operator=(vector &ref) {
